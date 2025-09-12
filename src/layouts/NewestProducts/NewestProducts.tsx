ed9e8feb03864bc24f7ec22814d83901243b7ffc
ArazMaher <Araz.maher.88@gmail.com>
2025-09-12 19:00:03 +0330

    Debug NewestProducts Props and Complete Home Main Picture Section

--- src/layouts/NewestProducts/NewestProducts.tsx
+++ src/layouts/NewestProducts/NewestProducts.tsx
@@ -2,19 +2,25 @@ import './NewestProducts.css'
 import NewestProductCard from '../../components/NewestProductCard/NewestProductCard'
 import MainBottom from '../../components/MainBottom/MainBottom'
 
-export default function NewestProducts() {
+type NewestProductsProps = {
+  title: string,
+  content: string,
+  firstImg: string,
+  secondImg: string
+}
+
+export default function NewestProducts({ title, content, firstImg, secondImg }: NewestProductsProps) {
   return (
     <div className='container'>
       <div className="newest-products">
-        <NewestProductCard img='../../images/Newest-Product-girl2.png' />
-        <NewestProductCard img='../../images/Newest-Product-girl.png' />
+        <NewestProductCard img={firstImg} />
+        <NewestProductCard img={secondImg} />
         <div className="newest-products__content">
-          <h1 className="newest-products__content-title">جدید ترین محصولات</h1>
+          <h1 className="newest-products__content-title">{title}</h1>
           <p className="newest-products__content-text">
-            جدید ترین محصولات با ارسال رایگان تهیه
-            کنید و در سریع ترین زمان درب منزل تحویل بگیرید
+            {content}
           </p>
-          <MainBottom bgColor='primary' title='مشاهده همه محصولات'/>
+          <MainBottom bgColor='primary' title='مشاهده همه محصولات' />
         </div>
       </div>
     </div>
