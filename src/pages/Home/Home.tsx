4bbdb5ff5e9ddbabe7f8b46039bda514eb18faf7
ArazMaher <Araz.maher.88@gmail.com>
2025-09-12 20:09:20 +0330

    Initialize Changes In Home and Fix Conflict

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Footer from "../../layouts/Footer/Footer"
 import Header from "../../layouts/Header/Header"
 import NewestProducts from "../../layouts/NewestProducts/NewestProducts"
 import HomeBanner from "../../components/HomeBanner/HomeBanner"
+import Pagination from "../../layouts/Pagination/Pagination"
 
 function Home() {
     return (
@@ -10,7 +11,10 @@ function Home() {
             <NewestProducts title="جدید ترین محصولات" content="جدید ترین محصولات با ارسال رایگان تهیه 
 کنید و در سریع ترین زمان درب منزل تحویل بگیرید" firstImg="../../images/Newest-Product-girl2.png" secondImg="../../images/Newest-Product-girl.png" />
             <HomeBanner bgSrc="../../../public/images/Home-Banner.jpg" title="برای حال خوب" content="لوازم ایروبیک و تناسب اندام بگیر" />
+            <Pagination title="تخفیفات ویژه"/>
+            <Pagination title="جدید ترین کفش های ورزشی "/>
             <HomeBanner bgSrc="../../../public/images/Home-Banner-2.jpg" title="برای حال خوب" content="لوازم ایروبیک و تناسب اندام بگیر" />
+            <Pagination title="تخفیفات ویژه"/>
             <NewestProducts title="پر فروش ترین محصولات" content="پرفروش ترین محصولات با ارسال رایگان تهیه 
 کنید و در سریع ترین زمان درب منزل تحویل بگیرید.
 " firstImg="../../images/Sale-Product-girl-1.png" secondImg="../../images/Sale-Product-girl-2.png" />
