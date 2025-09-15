6d6bd96c96a1d8539a93c70f7dac9ddb721977e5
ArazMaher <Araz.maher.88@gmail.com>
2025-09-15 14:20:32 +0330

    Completed Initial  Home Page In Desktop Size

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
+
+import './Home.css'
 import Footer from "../../layouts/Footer/Footer"
 import Header from "../../layouts/Header/Header"
 import NewestProducts from "../../layouts/NewestProducts/NewestProducts"
 import HomeBanner from "../../components/HomeBanner/HomeBanner"
 import Pagination from "../../layouts/Pagination/Pagination"
+import SiteOptions from '../../components/SIteOptions/SiteOptions'
 
 function Home() {
     return (
@@ -11,13 +14,17 @@ function Home() {
             <NewestProducts title="جدید ترین محصولات" content="جدید ترین محصولات با ارسال رایگان تهیه 
 کنید و در سریع ترین زمان درب منزل تحویل بگیرید" firstImg="../../images/Newest-Product-girl2.png" secondImg="../../images/Newest-Product-girl.png" />
             <HomeBanner bgSrc="../../../public/images/Home-Banner.jpg" title="برای حال خوب" content="لوازم ایروبیک و تناسب اندام بگیر" />
-            <Pagination title="تخفیفات ویژه"/>
-            <Pagination title="جدید ترین کفش های ورزشی "/>
+        <Pagination title="تخفیفات ویژه" bgImage='/images/Subtract.png'/>
+            <Pagination title="جدید ترین کفش های ورزشی " />
             <HomeBanner bgSrc="../../../public/images/Home-Banner-2.jpg" title="برای حال خوب" content="لوازم ایروبیک و تناسب اندام بگیر" />
-            <Pagination title="تخفیفات ویژه"/>
+            <Pagination title="تخفیفات ویژه" bgImage='images/Subtract2.png'/>
             <NewestProducts title="پر فروش ترین محصولات" content="پرفروش ترین محصولات با ارسال رایگان تهیه 
 کنید و در سریع ترین زمان درب منزل تحویل بگیرید.
 " firstImg="../../images/Sale-Product-girl-1.png" secondImg="../../images/Sale-Product-girl-2.png" />
+            <div className="camp">
+                <img src="../public/images/Ben10.png" alt="" />
+            </div>
+            <SiteOptions />
             <Footer />
         </>
     )
