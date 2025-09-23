7b43694af6da0c72bcedf31dbbb411bc246dca4e
ArazMaher <Araz.maher.88@gmail.com>
2025-09-23 18:10:56 +0330

    Complete Login and Register UI

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,14 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Products from "../pages/Products/Products";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const routes: RouteObject[] = [
     { path: "/", element: <Home /> },
     { path: "/products", element: <Products /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
 ]
 
 export default routes
