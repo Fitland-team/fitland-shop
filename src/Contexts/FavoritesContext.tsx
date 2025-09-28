cf77a1fa8b2bfff69444f5fb8ea81a803a9fae9a
ArazMaher <Araz.maher.88@gmail.com>
2025-09-28 13:36:52 +0330

    Finished Profile Comments Section

--- src/Contexts/FavoritesContext.tsx
+++ src/Contexts/FavoritesContext.tsx
@@ -1,36 +1,3 @@
-// // FavoritesContext.tsx
-// import { createContext, useContext, useState } from "react";
-// import type { ReactNode } from 'react'
-
-// type FavoritesContextType = {
-//   favorites: number[];
-//   toggleFavorite: (id: number) => void;
-// };
-
-// const FavoritesContext = createContext<FavoritesContextType | null>(null);
-
-// export const FavoritesProvider = ({ children }: { children: ReactNode }) => {
-//   const [favorites, setFavorites] = useState<number[]>([]);
-
-//   const toggleFavorite = (id: number) => {
-//     setFavorites((prev) =>
-//       prev.includes(id) ? prev.filter((fid) => fid !== id) : [...prev, id]
-//     );
-//   };
-
-//   return (
-//     <FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
-//       {children}
-//     </FavoritesContext.Provider>
-//   );
-// };
-
-// export const useFavorites = () => {
-//   const context = useContext(FavoritesContext);
-//   if (!context) throw new Error("useFavorites must be inside FavoritesProvider");
-//   return context;
-// };
-
 import { createContext, useContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
 
--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,7 +1,44 @@
+import { useState } from 'react'
 import './Comments.css'
+import CommentBox from '../CommentBox/CommentBox'
 
 export default function Comments() {
+
+  const [activeIndex, setActiveIndex] = useState<number>(0)
+
   return (
-    <div>Comments</div>
+    <div className='comments'>
+      <div className="comments-title">
+        <h2>دیدگاه‌ها و نظرات</h2>
+      </div>
+      <div className="comments-section">
+        <div className="comments-section__title">
+          <span className={`comments-section__title-list ${activeIndex === 0 ? 'active-list' : ''}`}
+            onClick={() => setActiveIndex(0)}
+          >نظرات تایید شده 2</span>
+          <span className={`comments-section__title-list ${activeIndex === 1 ? 'active-list' : ''}`}
+            onClick={() => setActiveIndex(1)}
+          >نظرات تایید نشده 3</span>
+        </div>
+        {
+          activeIndex === 0 && (
+            <div className="comments-box">
+              <CommentBox imgSrc='/images/bicy.png' title='دوچرخه آبی کلاسیک میلان2009 ' color='آبی-مشکی' desc='بسیار دوچرخه راحت و خوش قیمتیه. توی این مدت که سوار شدم هیچ نقصی ازش ندیدم به بقیه دوستان هم پیشنهاد میکنم خریداری کنند'/>
+              <CommentBox imgSrc='/images/approvedComment.png' title='ست ورزشی نایکی' color='مشکی' desc='دوخت بدی داشت و اونجور که توی تصویر نشون داده شده شیک نیست. دوخت ها بشدت مشخصه و حس بدی رو به مخاطب القا میکنه. خلاصه من از خریدم راضی'/>
+            </div>
+          )
+        }
+
+        {
+          activeIndex === 1 && (
+            <div className="not-confirmed__comments">
+              <CommentBox imgSrc='/images/NotConfirmed1.png' title='دمبل 2 کیلویی گرد مدل DHZ ' color='مشکی' desc='خوبه بخرین!'/>
+              <CommentBox imgSrc='/images/NotConfirmed2.png' title='توپ فوتبال مولتن سایز 5' color='سفید' desc='خیلیییی توپ خوبیه هم قیمت خوبی داره هم جنس خوب'/>
+              <CommentBox imgSrc='/images/NotConfirmed3.png' title='کفش ورزشی مردانه آونگ کد 83590753' color='سرمه ای' desc='کفش خوب و نرمی بود با تشکر از فیت لند'/>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
