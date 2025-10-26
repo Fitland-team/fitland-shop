2d3f2e6160caddb9f9bc5903b6e68b45bcca7250
ArazMaher <Araz.maher.88@gmail.com>
2025-10-26 21:14:13 +0330

    Finished Account and ProfileSideBar.tsx Responsive Section in 760px

--- src/layouts/ProfileSideBar/ProfileSideBar.tsx
+++ src/layouts/ProfileSideBar/ProfileSideBar.tsx
@@ -87,41 +87,42 @@ export default function ProfileSelection() {
                       fill="#292D32"
                     />
                   </svg>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="profile-upload"
+                    style={{ display: "none" }}
+                    onChange={handleImageChange}
+                    disabled={!isLoggedIn} // وقتی لاگ اوت بود غیر فعال
+                  />
+
+                  <label
+                    htmlFor="profile-upload"
+                    style={{ cursor: isLoggedIn ? "pointer" : "not-allowed" }}
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ cursor: "pointer" }}
+                      className="add-profile__mobile"
+                    >
+                      <path
+                        d="M18 12.75H6C5.59 12.75 5.25 12.41 5.25 12C5.25 11.59 5.59 11.25 6 11.25H18C18.41 11.25 18.75 11.59 18.75 12C18.75 12.41 18.41 12.75 18 12.75Z"
+                        fill="#000306"
+                      />
+                      <path
+                        d="M12 18.75C11.59 18.75 11.25 18.41 11.25 18V6C11.25 5.59 11.59 5.25 12 5.25C12.41 5.25 12.75 5.59 12.75 6V18C12.75 18.41 12.41 18.75 12 18.75Z"
+                        fill="#000306"
+                      />
+                    </svg>
+                  </label>
                 </div>
               )}
 
-              <input
-                type="file"
-                accept="image/*"
-                id="profile-upload"
-                style={{ display: "none" }}
-                onChange={handleImageChange}
-                disabled={!isLoggedIn} // وقتی لاگ اوت بود غیر فعال
-              />
 
-              <label
-                htmlFor="profile-upload"
-                style={{ cursor: isLoggedIn ? "pointer" : "not-allowed" }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  style={{ cursor: "pointer" }}
-                  className="add-profile__mobile"
-                >
-                  <path
-                    d="M18 12.75H6C5.59 12.75 5.25 12.41 5.25 12C5.25 11.59 5.59 11.25 6 11.25H18C18.41 11.25 18.75 11.59 18.75 12C18.75 12.41 18.41 12.75 18 12.75Z"
-                    fill="#000306"
-                  />
-                  <path
-                    d="M12 18.75C11.59 18.75 11.25 18.41 11.25 18V6C11.25 5.59 11.59 5.25 12 5.25C12.41 5.25 12.75 5.59 12.75 6V18C12.75 18.41 12.41 18.75 12 18.75Z"
-                    fill="#000306"
-                  />
-                </svg>
-              </label>
             </div>
 
             <h4 className="side-bar__profile-name">
